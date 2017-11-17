a322d9b2f80d1dd1f5104336794b3c05b8795c48
bitdog22 <glebdikarev@yahoo.com>
2017-11-17 02:20:55 +0100

    edited 2am

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -5,30 +5,52 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit 
+{
 
-  user_title: string;
+  name: string;
   hobbies: any[];
   address: Address;
   
-  constructor() { }
+  constructor() 
+  { 
+  
+  }
 
-  ngOnInit() {
-    this.user_title = 'Hlib';
-    this.address = {
-    street: '50 main st',
-    city: 'NewYork',
-    state: 'NY'
+  ngOnInit() 
+  {
+    this.name = 'Hlib';
+    this.address = 
+    {
+      street: '50 main st',
+      city: 'NewYork',
+      state: 'NY'
     }
     this.hobbies = ['Snowboarding', 'Crypro'];
   }
   
-  onClick() {
-  console.log('Click worked!');
+  
+  addHobby(hobby)
+  {
+    this.hobbies.push(hobby);
+    return false;
+  }
+  
+  deleteHobby(hobby)
+  {
+    for (let i = 0; i < this.hobbies.length; i++)
+    {
+      if (this.hobbies[i] == hobby)
+        {
+          this.hobbies.splice(i, 1);
+        }
+    }
   }
   
 }
 
+ 
+}
 interface Address {
   street: string;
   city: string;
